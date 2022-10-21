7589cc16c0a737149e852b02f62c147b81ca1166
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-10-21 18:13:04 +0200

    Tp 2 fin de l'avancement partie 1

--- TP2_Bieres_Zavarella/src/main/java/com/mycompany/tp2_bieres_zavarella/TP2_Bieres_Zavarella.java
+++ TP2_Bieres_Zavarella/src/main/java/com/mycompany/tp2_bieres_zavarella/TP2_Bieres_Zavarella.java
@@ -13,22 +13,47 @@ public class TP2_Bieres_Zavarella {
 
     public static void main(String[] args) {
         
-       BouteilleBiere uneBiere = new BouteilleBiere();
+       BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des Trolls", 7.0, "Dubuisson");
        //uneBiere.Nom = "Cuvee des Trolls";
        //uneBiere.degreAlcool = 7.0;
        //uneBiere.brasserie = "Dubuisson";
        //uneBiere.ouverte = false;
+       uneBiere.ouverte = false;
        uneBiere.lireEtiquette();
+       System.out.println(uneBiere);
        
-       BouteilleBiere uneBiere2 = new BouteilleBiere();
-       uneBiere2.Nom = "leffe";
-       uneBiere2.degreAlcool = 6.6;
-       uneBiere2.brasserie = "Abbaye de Leffe";
-       uneBiere2.ouverte = false;
+       System.out.println("\n");
+               
+       BouteilleBiere uneBiere2 = new BouteilleBiere("leffe", 6.6, "Abbaye de Leffe");
+       //uneBiere2.Nom = "leffe";
+       //uneBiere2.degreAlcool = 6.6;
+       //uneBiere2.brasserie = "Abbaye de Leffe";
+       uneBiere2.ouverte = true;
        uneBiere2.lireEtiquette();
+       uneBiere2.Décapsuler();
+       System.out.println(uneBiere2);
        
-       BouteilleBiere Biere1664 = new BouteilleBiere(unNom: "1664", unDegre:6.1, uneBrasserie:"LaTour");
+       System.out.println("\n");
+       
+       BouteilleBiere Biere1664 = new BouteilleBiere("1664", 5.2, "Latour");
+       Biere1664.Décapsuler();
        Biere1664.lireEtiquette();
+       System.out.println(Biere1664);
+       
+       System.out.println("\n");
+       
+       BouteilleBiere BiereDesperados = new BouteilleBiere("Desperados", 6.2, "IO");
+       BiereDesperados.lireEtiquette();
+       System.out.println(BiereDesperados);
+       
+       System.out.println("\n");
+       
+       BouteilleBiere BiereSkoll = new BouteilleBiere("Skoll", 8.0, "Leviatan");
+       BiereSkoll.Décapsuler();
+       BiereSkoll.lireEtiquette();
+       System.out.println(BiereSkoll);
+       
+       
        
     }
 }
