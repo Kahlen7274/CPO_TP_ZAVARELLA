8ec7d86bb0fcf9f62aab1d61e93caae89d2999f6
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-10-25 12:37:16 +0200

    TP1 parite 2, ajout des conditions pour choisir ces convertions

--- TP1_convertisseur_ZAVARELLA_MATTEO/src/main/java/com/mycompany/tp1_convertisseur_zavarella_matteo/TP1_convertisseur_ZAVARELLA_MATTEO.java
+++ TP1_convertisseur_ZAVARELLA_MATTEO/src/main/java/com/mycompany/tp1_convertisseur_zavarella_matteo/TP1_convertisseur_ZAVARELLA_MATTEO.java
@@ -21,9 +21,56 @@ public class TP1_convertisseur_ZAVARELLA_MATTEO {
         // Affichage utilisateur
         System.out.println("Entrer une temperature: ");
         Double Temp = sc.nextDouble(); // Stock la valeur saisie
+        System.out.println("""
+                           Saisissez la conversion que vous souhaiter effectuer : 
+                            1) De Celsius vers Kelvin 
+                            2) De Kelvin vers Celsius 
+                            3) De Farenheit vers Kelvin
+                            4) De Kelvin vers Fahrenheit
+                            5) De Fahrenheit vers Celsius
+                            6) De Celsius vers Fahrenheit
+                           """);
+        int Conv = sc.nextInt();
         
         // Appelle de la classe convertisseur
         Convertisseurtemp TempConv = new Convertisseurtemp();
+        
+        if (Conv == 1){
+           double k;
+           k = TempConv.CelsiusVersKelvin(Temp);
+           System.out.println(Temp + " degré celsius est égal à " + k +" degré kelvin");
+        }
+        
+        if (Conv == 2){
+            double c;
+            c = TempConv.KelvinVersCelsius(Temp);
+            System.out.println(Temp + " degré kelvin est égal à " + c +" degré celsius");
+        }
+        
+        if (Conv == 3){
+           double k;
+           k = TempConv.FahrenheitVersKelvin(Temp);
+           System.out.println(Temp + " degré fahrenheit est égal à " + k +" degré kelvin");
+        }
+        
+        if (Conv == 4){
+           double f;
+           f = TempConv.KelvinVersFahrenheit(Temp);
+           System.out.println(Temp + " degré kelvin est égal à " + f +" degré fahrenhiet");
+        }
+        
+        if (Conv == 5){
+           double c;
+           c = TempConv.FahrenheitVersCelsius(Temp);
+           System.out.println(Temp + " degré fahrenheit est égal à " + c +" degré celsius");
+        }
+        
+        if (Conv == 6){
+           double f;
+           f = TempConv.CelsiusVersFahrenheit(Temp);
+           System.out.println(Temp + " degré celsius est égal à " + f +" degré fahrenheit");
+        }
+        
         //TempConv.CelsiusVersKelvin(Temp);
         //TempConv.FahrenheitVersCelsius(Temp);
         //TempConv.CelsiusVersFahrenheit(Temp);
