c2f8e86171cc7006d460ae11b45bb492a2fa6162
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-10-25 13:09:30 +0200

    TP1 parite 2, ajout des conditions pour choisir ces convertions

--- TP1_convertisseur_ZAVARELLA_MATTEO/src/main/java/com/mycompany/tp1_convertisseur_zavarella_matteo/Convertisseurtemp.java
+++ TP1_convertisseur_ZAVARELLA_MATTEO/src/main/java/com/mycompany/tp1_convertisseur_zavarella_matteo/Convertisseurtemp.java
@@ -9,12 +9,59 @@ package com.mycompany.tp1_convertisseur_zavarella_matteo;
  * @author matte
  */
 public class Convertisseurtemp {
+
+    // On crée la variable
+    int nbConvertions;
+
+    public int Convertions() {
+        nbConvertions = 0;
+        // Methode pour convertir Kelvin en Celsius
+    }
     
-    public double CelciusVersKelvin (double tCelcius){ 
-        // Methode pour convertir les Celcius en Kelvin
-            double K = tCelcius + 273.15;
-            System.out.println("Votre temperature en Kelvin est: " + K);
-            return K;
-        }
-    
+    public double CelsiusVersKelvin(double tCelsius) {
+        double K = tCelsius + 273.15;
+        System.out.println("Votre temperature en Kelvin est: " + K);
+        return K;
+    }
+    // Methode pour convertir Celsius en Kelvin
+
+    public double KelvinVersCelsius(double tKelvin) {
+        double C = tKelvin - 273.15;
+        System.out.println("Votre temperature en Kelvin est: " + C);
+        return C;
+    }
+
+    // Methode pour convertir Fahrenheit en Celsius
+    public double FahrenheitVersCelsius(double tFahrenheit) {
+        double C = (tFahrenheit - 32) * 5 / 9;
+        System.out.println("Votre temperature en Celsius est: " + C);
+        return C;
+    }
+
+    // Methode pour convertir Celcius en Fahrenheit
+    public double CelsiusVersFahrenheit(double tCelsius) {
+        double F = (tCelsius * 9 / 5) + 32;
+        System.out.println("Votre temperature en Fahrenheit est: " + F);
+        return F;
+    }
+
+    // Methode pour convertir Kelvin en Fahrenheit
+    public double KelvinVersFahrenheit(double tKelvin) {
+        double F = (tKelvin - 273.15) * 9 / 5 + 32;
+        System.out.println("Votre temperature en Fahrenheit est: " + F);
+        return F;
+    }
+
+    // Method to convert Fahrenheit to Kelvin
+    public double FahrenheitVersKelvin(double tFahrenheit) {
+        double K = (tFahrenheit - 32) * 5 / 9 + 273.15;
+        System.out.println("Votre temperature en Kelvin est: " + K);
+        return K;
+    }
+
+    // Methode qui retourne le nombre de conversions effectuées
+    @Override
+    public String toString() {
+        return "nb de conversions" + nbConvertions;
+    }
 }
