0737a069cd6791ad78ffa985826c56c17acac436
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-11-08 17:03:43 +0100

    TP3 creation du tableau dynamique et test

--- TP3_Heritage, classe abstraite, interface/src/tp3_heritage/classe/abstraite/pkginterface/TP3_HeritageClasseAbstraiteInterface.java
+++ TP3_Heritage, classe abstraite, interface/src/tp3_heritage/classe/abstraite/pkginterface/TP3_HeritageClasseAbstraiteInterface.java
@@ -8,6 +8,8 @@ package tp3_heritage.classe.abstraite.pkginterface;
  *
  * @author matte
  */
+import java.util.ArrayList; // import la classe arraylist
+
 public class TP3_HeritageClasseAbstraiteInterface {
 
     /**
@@ -16,16 +18,28 @@ public class TP3_HeritageClasseAbstraiteInterface {
     public static void main(String[] args) {
         
         Epée arme1 = new Epée("Excalibur", 7,5);
-        System.out.println(arme1);
+        //System.out.println(arme1);
         
         Epée arme2 = new Epée("Durandal", 4,7);
-        System.out.println(arme2);
+        //System.out.println(arme2);
         
         Baton arme3 = new Baton("Chene",4,5);
-        System.out.println(arme3);
+        //System.out.println(arme3);
         
         Baton arme4 = new Baton("Charme",5,6);
-        System.out.println(arme4);
+        //System.out.println(arme4);
+        
+        
+        ArrayList<Arme> Tab = new ArrayList<Arme>();
+        Tab.add(arme1);
+        Tab.add(arme2);
+        Tab.add(arme3);
+        Tab.add(arme4);
+        System.out.println(Tab);
+        
+        int tailleDeTab = Tab.size(); // on récupère la taille          
+        System.out.println("la taille de Tab est " + tailleDeTab); 
+        
     }
     
 }
