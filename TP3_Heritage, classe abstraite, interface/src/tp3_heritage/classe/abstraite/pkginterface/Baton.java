154fbdfb703d78197324fb1092bd55706449529d
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-11-08 15:38:41 +0100

    TP3 création classe Baton

---
+++ TP3_Heritage, classe abstraite, interface/src/tp3_heritage/classe/abstraite/pkginterface/Baton.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heritage.classe.abstraite.pkginterface;
+
+/**
+ *
+ * @author matte
+ */
+public class Baton extends Arme{
+    int age;
+    
+    public Baton(String nom, int ATQ, int a){
+        super(nom, ATQ);
+                age = a;
+    }
+    
+}
