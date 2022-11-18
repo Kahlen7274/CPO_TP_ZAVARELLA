b0fb9c261f473bed39c86ca202591a6db06dde14
Matteo Zavarella <matteo.zavarella@epfedu.fr>
2022-11-18 10:40:04 +0100

    Super puissance 4 (V1) création du projet et de la classe jeton

---
+++  SP4_console_Zavarella_Matteo/src/sp4_console_zavarella_matteo/SP4_console_Zavarella_Matteo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_zavarella_matteo;
+
+/**
+ *
+ * @author matte
+ */
+public class SP4_console_Zavarella_Matteo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
